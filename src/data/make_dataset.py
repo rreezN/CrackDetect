ec47dd360073ba51e50a0d607b1f63d0cea27df1
DCZ <dennischenxi@gmail.com>
2024-02-16 11:24:24 +0100

    Refactor make_dataset.py to unpack and convert data from hdf5 files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
@@ -5,33 +6,45 @@ import os
 import random
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
-import hdf5
+import h5py
 
-class Converters:
-    @staticmethod
-    def median_lambda(df: pd.DataFrame, column_name) -> pd.DataFrame:
-        return df.groupby(df.index // 10)[column_name].median()
+parameter_dict = {
+        'acc_long':     {'bstar': 198,      'rstar': 1,     'b': 198,   'r': 0.05   },
+        'acc_trans':    {'bstar': 32768,    'rstar': 1,     'b': 32768, 'r': 0.04   },
+        'acc_yaw':      {'bstar': 2047,     'rstar': 1,     'b': 2047,  'r': 0.1    },
+        'brk_trq_elec': {'bstar': 4096,     'rstar': -1,    'b': 4098,  'r': -1     },
+        'whl_trq_est':  {'bstar': 12800,    'rstar': 0.5,   'b': 12700, 'r': 1      },
+        'trac_cons':    {'bstar': 80,       'rstar': 1,     'b': 79,    'r': 1      },
+        'trip_cons':    {'bstar': 0,        'rstar': 0.1,   'b': 0,     'r': 1      }
+    }
 
-    # Create formula for the generic conversion formula, and add b*, b and r*, r as inputs
+def convertdata(data, parameter):
+    bstar = parameter['bstar']
+    rstar = parameter['rstar']
+    b = parameter['b']
+    r = parameter['r']
+    out = ((data - bstar*rstar) - b)*r
+    return out
 
 
-# Read csv file and return the data
-def read_csv(file_path) -> list[str]:  
-    with open(file_path, 'r') as file:
-        data = file.readlines()
+def unpack_hdf5(hdf5_file):
+    with h5py.File(hdf5_file, 'r') as f:
+        data = unpack_hdf5_(f)
     return data
 
-def load_data() -> list:
-    
-    platoon_hh = hdf5.load('../../data/raw/ref_data/cph1_aran_hh.csv')
-    platoon_vh = hdf5.load('../../data/raw/ref_data/cph1_aran_vh.csv')
-    return [platoon_hh, platoon_vh]
-
-def main():
-
-    data = load_data()
-    convert = Converters()
+def unpack_hdf5_(group):
+    data = {}
+    for key in group.keys():
+        if isinstance(group[key], h5py.Group):
+            data[key] = unpack_hdf5_(group[key])
+        else:
+            if key in parameter_dict.keys():
+                data[key] = convertdata(group[key][()], parameter_dict[key])
+            else:
+                data[key] = group[key][()]
+    return data
 
 if __name__ == '__main__':
-    # Get the data and process it
-    pass
+    autopi_hh = unpack_hdf5('data/raw/AutoPi_CAN/platoon_CPH1_HH.hdf5')
+    autopi_vh = unpack_hdf5('data/raw/AutoPi_CAN/platoon_CPH1_VH.hdf5')
+    print('Data unpacked and converted...')
