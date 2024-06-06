e06d8543e3d2e86cc55225a7d01b7cd320b2c6e3
KrisMarboe <kristoffermarboe@gmail.com>
2024-06-06 15:15:34 +0200

    clean up in resample

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -709,6 +709,14 @@ def resample_gopro(section: h5py.Group, resampled_distances: np.ndarray) -> dict
             new_section[key] = interpolate(measurement_distances[name], value[()], resampled_distances)
     return new_section
 
+def extract_bit_data(segment: h5py.Group, start: int, end: int) -> tuple[np.ndarray, dict[str, int]]:
+    bit_data = np.zeros((end - start, len(segment.keys())))
+    bit_attributes = {}
+    for i, (key, value) in enumerate(segment.items()):
+        bit_data[:, i] = value[start: end]
+        bit_attributes[key] = i
+    return bit_data, bit_attributes
+
 def resample(verbose: bool = False) -> None:
     # Resample the gm data to a fixed frequency
     frequency = 250
@@ -724,112 +732,93 @@ def resample(verbose: bool = False) -> None:
     segment_path = Path(f'data/processed/wo_kpis/segments.hdf5')
     if segment_path.exists():
         segment_path.unlink()
-    
-    # Load raw reference data
-    aran = {
-        'hh': pd.read_csv('data/raw/ref_data/cph1_aran_hh.csv', sep=';', encoding='unicode_escape').fillna(0).select_dtypes(include=np.number),
-        'vh': pd.read_csv('data/raw/ref_data/cph1_aran_vh.csv', sep=';', encoding='unicode_escape').fillna(0).select_dtypes(include=np.number)
-    }
-
-    p79 = {
-        'hh': pd.read_csv('data/raw/ref_data/cph1_zp_hh.csv', sep=';', encoding='unicode_escape').select_dtypes(include=np.number),
-        'vh': pd.read_csv('data/raw/ref_data/cph1_zp_vh.csv', sep=';', encoding='unicode_escape').select_dtypes(include=np.number)
-    }
 
     # Resample the data
-    with h5py.File(gm_segment_file, 'r') as f:
-        segment_files = [f[str(i)] for i in range(len(f))]
+    with h5py.File(gm_segment_file, 'r') as gm:
+        segment_files = [gm[str(i)] for i in range(len(gm))]
         pbar = tqdm(segment_files)
         # Load gopro data
-        with h5py.File('data/interim/gopro/segments.hdf5', 'r') as f2:
-            # Open final processed segments file
-            with h5py.File('data/processed/wo_kpis/segments.hdf5', 'a') as f3:
-                for i, segment in enumerate(pbar):
-                    pbar.set_description(f"Resampling segment {i+1:03d}/{len(segment_files)}")
-                    segment_subgroup = f3.create_group(str(i))
+        with h5py.File('data/interim/gopro/segments.hdf5', 'r') as gopro:
+            # Load ARAN and P79 data
+            with h5py.File('data/interim/aran/segments.hdf5', 'r') as aran:
+                with h5py.File('data/interim/p79/segments.hdf5', 'r') as p79:
+                    # Open final processed segments file
+                    with h5py.File('data/processed/wo_kpis/segments.hdf5', 'a') as wo_kpis:
+                        for i, segment in enumerate(pbar):
+                            pbar.set_description(f"Resampling segment {i+1:03d}/{len(segment_files)}")
+                            segment_subgroup = wo_kpis.create_group(str(i))
 
-                    # Add direction, trip name and pass name as attr to segment subgroup
-                    segment_subgroup.attrs['direction'] = segment['direction'][()].decode("utf-8")
-                    segment_subgroup.attrs['trip_name'] = segment["trip_name"][()].decode('utf-8')
-                    segment_subgroup.attrs['pass_name'] = segment["pass_name"][()].decode('utf-8')
+                            # Add direction, trip name and pass name as attr to segment subgroup
+                            segment_subgroup.attrs['direction'] = segment['direction'][()].decode("utf-8")
+                            segment_subgroup.attrs['trip_name'] = segment["trip_name"][()].decode('utf-8')
+                            segment_subgroup.attrs['pass_name'] = segment["pass_name"][()].decode('utf-8')
 
-                    # Get relevant reference data
-                    direction = segment['direction'][()].decode("utf-8")
-                    aran_dir = aran[direction]
-                    p79_dir = p79[direction]
+                            # Get relevant reference data
+                            aran_segment = aran[str(i)]
+                            aran_segment_lonlat = np.column_stack((aran_segment['Lon'], aran_segment['Lat']))
+                            p79_segment = p79[str(i)]
+                            p79_segment_lonlat = np.column_stack((p79_segment['Lon'], p79_segment['Lat']))
 
-                    # Resample the GM data
-                    resampled_gm_segment = resample_gm(segment, frequency=frequency)
-                    resampled_distances = resampled_gm_segment["distance"]
+                            # Resample the GM data
+                            resampled_gm_segment = resample_gm(segment, frequency=frequency)
+                            resampled_distances = resampled_gm_segment["distance"]
+                            gm_segment_lonlat = np.column_stack((resampled_gm_segment['gps_1'], resampled_gm_segment['gps_0']))
 
-                    # Cut the aran and p79 data by the lonlat of the segment
-                    bit_lon = resampled_gm_segment['gps_1']
-                    bit_lat = resampled_gm_segment['gps_0']
-                    bit_lonlat = np.column_stack((bit_lon, bit_lat))
-                    aran_segment_match = find_best_start_and_end_indeces_by_lonlat(aran_dir[["Lon", "Lat"]].values, bit_lonlat)
-                    aran_segment = cut_dataframe_by_indeces(aran_dir, *aran_segment_match)
+                            # resample the gopro data
+                            gopro_data_exists = False
+                            if str(i) in gopro.keys():
+                                gopro_data_exists = True
+                                gopro_segment = gopro[str(i)]
+                                resampled_gopro_segment = resample_gopro(gopro_segment, resampled_distances)
 
-                    p79_segment_match = find_best_start_and_end_indeces_by_lonlat(p79_dir[["Lon", "Lat"]].values, bit_lonlat)
-                    p79_segment = cut_dataframe_by_indeces(p79_dir, *p79_segment_match)
+                            # Cut segments into 1 second bits
+                            steps = (len(resampled_distances) // (frequency * seconds_per_step))
+                            for j in range(steps):
+                                start = j*frequency*seconds_per_step
+                                end = (j+1)*frequency*seconds_per_step
+                                time_subgroup = segment_subgroup.create_group(str(j))
+                                
+                                # concatenate the measurements for each 1 second bit
+                                gm_measurements = []
+                                gm_attributes = {}
+                                for i, (measurement_key, measurement_value) in enumerate(resampled_gm_segment.items()):
+                                    gm_attributes[measurement_key] = i
+                                    gm_measurements.append(measurement_value[start: end])
+                                gm_measurements = np.column_stack(gm_measurements)
+                                # Save the resampled GM data in groups of 'frequency' length
+                                gm_dataset = time_subgroup.create_dataset("gm", data=gm_measurements)
+                                gm_dataset.attrs.update(gm_attributes)
 
-                    # resample the gopro data
-                    gopro_data_exists = False
-                    if str(i) in f2.keys():
-                        gopro_data_exists = True
-                        gopro_segment = f2[str(i)]
-                        resampled_gopro_segment = resample_gopro(gopro_segment, resampled_distances)
+                                if gopro_data_exists:
+                                    # save the resampled gopro data in groups of 'frequency' length
+                                    gopro_measurements = []
+                                    gopro_attributes = {}
+                                    for i, (key, value) in enumerate(resampled_gopro_segment.items()):
+                                        values = value[start: end]
+                                        gopro_attributes[key] = i
+                                        gopro_measurements.append(values)
+                                    gopro_measurements = np.column_stack(gopro_measurements)
+                                    gopro_dataset = time_subgroup.create_dataset("gopro", data=gopro_measurements)
+                                    gopro_dataset.attrs.update(gopro_attributes)
+                    
+                                # Find the corresponding ARAN and P79 data for each 1 second bit using closest lonlat points
+                                bit_lonlat = gm_segment_lonlat[start: end]
 
-                    # Cut segments into 1 second bits
-                    steps = (len(resampled_distances) // (frequency * seconds_per_step))
-                    for j in range(steps):
-                        start = j*frequency*seconds_per_step
-                        end = (j+1)*frequency*seconds_per_step
-                        time_subgroup = segment_subgroup.create_group(str(j))
-                        
-                        # concatenate the measurements for each 1 second bit
-                        gm_measurements = []
-                        gm_attributes = {}
-                        for i, (measurement_key, measurement_value) in enumerate(resampled_gm_segment.items()):
-                            gm_attributes[measurement_key] = i
-                            gm_measurements.append(measurement_value[start: end])
-                        gm_measurements = np.column_stack(gm_measurements)
-                        # Save the resampled GM data in groups of 'frequency' length
-                        gm_dataset = time_subgroup.create_dataset("gm", data=gm_measurements)
-                        gm_dataset.attrs.update(gm_attributes)
+                                aran_match_start, aran_match_end = find_best_start_and_end_indeces_by_lonlat(aran_segment_lonlat, bit_lonlat)
+                                aran_bit_data, aran_bit_attributes = extract_bit_data(aran_segment, aran_match_start, aran_match_end)
+                                aran_dataset = time_subgroup.create_dataset("aran", data=aran_bit_data)
+                                aran_dataset.attrs.update(aran_bit_attributes)
+                                aran_counts.append(aran_match_end - aran_match_start)
 
-                        if gopro_data_exists:
-                            # save the resampled gopro data in groups of 'frequency' length
-                            gopro_measurements = []
-                            gopro_attributes = {}
-                            for i, (key, value) in enumerate(resampled_gopro_segment.items()):
-                                values = value[start: end]
-                                gopro_attributes[key] = i
-                                gopro_measurements.append(values)
-                            gopro_measurements = np.column_stack(gopro_measurements)
-                            gopro_dataset = time_subgroup.create_dataset("gopro", data=gopro_measurements)
-                            gopro_dataset.attrs.update(gopro_attributes)
-            
-                        # Find the corresponding ARAN and P79 data for each 1 second bit using closest lonlat points
-                        bit_lonlat_time = bit_lonlat[start: end]
-                        aran_match_bit = find_best_start_and_end_indeces_by_lonlat(aran_segment[["Lon", "Lat"]].values, bit_lonlat_time)
-                        aran_bit = cut_dataframe_by_indeces(aran_segment, *aran_match_bit).values
-                        aran_columns = aran_segment.columns
-                        aran_dataset = time_subgroup.create_dataset("aran", data=aran_bit)
-                        for i, column in enumerate(aran_columns):
-                            aran_dataset.attrs[column] = i
-                        aran_counts.append(len(aran_bit))
+                                p79_match_start, p79_match_end = find_best_start_and_end_indeces_by_lonlat(p79_segment_lonlat, bit_lonlat)
+                                p79_bit_data, p79_bit_attributes = extract_bit_data(p79_segment, p79_match_start, p79_match_end)
+                                p79_dataset = time_subgroup.create_dataset("p79", data=p79_bit_data)
+                                p79_dataset.attrs.update(p79_bit_attributes)
+                                p79_counts.append(p79_match_end - p79_match_start)
 
-                        p79_match_bit = find_best_start_and_end_indeces_by_lonlat(p79_segment[["Lon", "Lat"]].values, bit_lonlat_time)
-                        p79_bit = cut_dataframe_by_indeces(p79_segment, *p79_match_bit).values
-                        p79_columns = p79_segment.columns
-                        p79_dataset = time_subgroup.create_dataset("p79", data=p79_bit)
-                        for i, column in enumerate(p79_columns):
-                            p79_dataset.attrs[column] = i
-                        p79_counts.append(len(p79_bit))
-
-                        # Plot the longitude and lattitude coordinates of the gm segment and the matched ARAN and P79 data
-                        if verbose and (aran_match_bit[1] - aran_match_bit[0] < 3 or p79_match_bit[1] - p79_match_bit[0] < 3):
-                            verbose_resample_plot(bit_lonlat_time, aran_segment, aran_match_bit, p79_segment, p79_match_bit)
+                                # Plot the longitude and lattitude coordinates of the gm segment and the matched ARAN and P79 data
+                                if verbose (aran_counts[-1] < 3 or p79_counts[-1] < 3):
+                                    verbose_resample_plot(bit_lonlat, aran_segment_lonlat, (aran_match_start, aran_match_end), p79_segment_lonlat, (p79_match_start, p79_match_end))
     
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     axes[0].hist(aran_counts, bins=20)
@@ -843,31 +832,32 @@ def resample(verbose: bool = False) -> None:
     plt.tight_layout()
     plt.show()
 
-def verbose_resample_plot(bit_lonlat_time, aran_segment, aran_match_bit, p79_segment, p79_match_bit):
+def verbose_resample_plot(bit_lonlat, aran_segment_lonlat, aran_match_bit, p79_segment_lonlat, p79_match_bit):
     fig, ax = plt.subplots()
-    ax.plot(bit_lonlat_time[:, 0], bit_lonlat_time[:, 1], label='GM', c='k')
+    ax.plot(bit_lonlat[:, 0], bit_lonlat[:, 1], label='GM', c='k')
     # Extract ARAN and P79 with n extra points on each side for better visualization
     n = 3
     aran_match_before = max(0, aran_match_bit[0] - n)
-    aran_match_after = min(len(aran_segment), aran_match_bit[1] + n)
-    wider_aran_bit = aran_segment[["Lon", "Lat"]].values[aran_match_before: aran_match_after]
-    shallow_aran_bit = aran_segment[["Lon", "Lat"]].values[aran_match_bit[0]: aran_match_bit[1]]
-    p79_match_before = max(0, p79_match_bit[0] - n)
-    p79_match_after = min(len(p79_segment), p79_match_bit[1] + n)
-    wider_p79_bit = p79_segment[["Lon", "Lat"]].values[p79_match_before: p79_match_after]
-    shallow_p79_bit = p79_segment[["Lon", "Lat"]].values[p79_match_bit[0]: p79_match_bit[1]]
+    aran_match_after = min(len(aran_segment_lonlat), aran_match_bit[1] + n)
+    wider_aran_bit = aran_segment_lonlat[aran_match_before: aran_match_after]
+    shallow_aran_bit = aran_segment_lonlat[aran_match_bit[0]: aran_match_bit[1]]
     ax.plot(wider_aran_bit[:, 0], wider_aran_bit[:, 1], label='ARAN', linestyle='--', alpha=0.5)
     ax.scatter(shallow_aran_bit[:, 0], shallow_aran_bit[:, 1], alpha=0.5, marker='x')
     if aran_match_before < aran_match_bit[0]:
-        ax.scatter(aran_segment[["Lon", "Lat"]].values[aran_match_before: aran_match_bit[0], 0], aran_segment[["Lon", "Lat"]].values[aran_match_before: aran_match_bit[0], 1], alpha=0.5, marker='x', c='r')
+        ax.scatter(aran_segment_lonlat[aran_match_before: aran_match_bit[0], 0], aran_segment_lonlat[aran_match_before: aran_match_bit[0], 1], alpha=0.5, marker='x', c='r')
     if aran_match_after > aran_match_bit[1]:
-        ax.scatter(aran_segment[["Lon", "Lat"]].values[aran_match_bit[1]: aran_match_after, 0], aran_segment[["Lon", "Lat"]].values[aran_match_bit[1]: aran_match_after, 1], alpha=0.5, marker='x', c='r')
+        ax.scatter(aran_segment_lonlat[aran_match_bit[1]: aran_match_after, 0], aran_segment_lonlat[aran_match_bit[1]: aran_match_after, 1], alpha=0.5, marker='x', c='r')
+
+    p79_match_before = max(0, p79_match_bit[0] - n)
+    p79_match_after = min(len(p79_segment_lonlat), p79_match_bit[1] + n)
+    wider_p79_bit = p79_segment_lonlat[p79_match_before: p79_match_after]
+    shallow_p79_bit = p79_segment_lonlat[p79_match_bit[0]: p79_match_bit[1]]
     ax.plot(wider_p79_bit[:, 0], wider_p79_bit[:, 1], label='P79', linestyle='--', alpha=0.5)
     ax.scatter(shallow_p79_bit[:, 0], shallow_p79_bit[:, 1], alpha=0.5, marker='x')
     if p79_match_before < p79_match_bit[0]:
-        ax.scatter(p79_segment[["Lon", "Lat"]].values[p79_match_before: p79_match_bit[0], 0], p79_segment[["Lon", "Lat"]].values[p79_match_before: p79_match_bit[0], 1], alpha=0.5, marker='x', c='r')
+        ax.scatter(p79_segment_lonlat[p79_match_before: p79_match_bit[0], 0], p79_segment_lonlat[p79_match_before: p79_match_bit[0], 1], alpha=0.5, marker='x', c='r')
     if p79_match_after > p79_match_bit[1]:
-        ax.scatter(p79_segment[["Lon", "Lat"]].values[p79_match_bit[1]: p79_match_after, 0], p79_segment[["Lon", "Lat"]].values[p79_match_bit[1]: p79_match_after, 1], alpha=0.5, marker='x', c='r')
+        ax.scatter(p79_segment_lonlat[p79_match_bit[1]: p79_match_after, 0], p79_segment_lonlat[p79_match_bit[1]: p79_match_after, 1], alpha=0.5, marker='x', c='r')
     # Place legend outside of plot
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     ax.set_aspect('equal', adjustable='box')
