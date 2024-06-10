37b276c507a9a6fe00907f3f3ce8a90cf537da21
DavidAriOstenfeldt <davidostenfeldt@hotmail.com>
2024-06-10 13:31:33 +0200

    predict_model: update to current outputs

--- src/predict_model.py
+++ src/predict_model.py
@@ -55,7 +55,7 @@ def predict(model: torch.nn.Module, testloader: torch.utils.data.DataLoader):
     return all_predictions, all_targets, test_losses
 
 
-def calculate_errors(predictions: torch.Tensor, targets: torch.Tensor, lags: int = 10):
+def calculate_errors(predictions, targets, lags: int = 10):
     """Calculate the errors between the predictions and the targets.
     
     Parameters:
@@ -70,6 +70,11 @@ def calculate_errors(predictions: torch.Tensor, targets: torch.Tensor, lags: int
         best_correlation (tuple): The best correlation between the predictions and the targets for each KPI.
     """
     
+    if isinstance(targets, torch.Tensor):
+        targets = targets.detach().numpy()
+    if isinstance(predictions, torch.Tensor):
+        predictions = predictions.detach().numpy()
+    
     # Calculate RMSE between targets and predictions for each KPI
     RMSE = np.sqrt(np.mean(np.abs(targets - predictions)**2, axis=0))
     
@@ -78,18 +83,28 @@ def calculate_errors(predictions: torch.Tensor, targets: torch.Tensor, lags: int
     
     # Calculate correlation between targets and predictions for each KPI with different lags
     correlations = []
-    for lag in range(1, lags):
-        correlation = np.corrcoef(targets[:-lag], predictions[lag:])[0, 1]
-        correlations.append(correlation)
+    for lag in range(0, lags):
+        for i in range(targets.shape[1]):
+            if lag == 0:
+                correlation = np.corrcoef(targets[:, i], predictions[:, i])[0, 1]
+            else:
+                correlation = np.corrcoef(targets[:-lag, i], predictions[lag:, i])[0, 1]
+                
+            correlations.append(correlation)
     
-    # Save best correlation
-    best_correlation = np.max(correlations)
-    best_correlation_lag = np.argmax(correlations) + 1
+    # Save best correlation for each target
+    best_correlations = np.zeros(targets.shape[1])
+    best_lags = np.zeros(targets.shape[1])
     
-    return RMSE, baseline_RMSE, (best_correlation_lag, best_correlation)
+    # find the best correlation and lag for each KPI
+    for i in range(targets.shape[1]):
+        best_correlations[i] = max(correlations[i::lags])
+        best_lags[i] = np.argmax(correlations[i::lags])
+    
+    return RMSE, baseline_RMSE, (best_lags, best_correlations)
 
 
-def plot_predictions(predictions, targets, test_losses, show=False):
+def plot_predictions(predictions: torch.Tensor, targets: torch.Tensor, test_losses: np.ndarray, show: bool = False):
     """Plot the predictions against the targets. Also reports the RMSE and correlation between the predictions and the targets.
 
     Args:
@@ -98,6 +113,9 @@ def plot_predictions(predictions, targets, test_losses, show=False):
         test_losses (_type_): _description_
         show (bool, optional): _description_. Defaults to False.
     """
+    
+    predictions = predictions.detach().numpy()
+    targets = targets.detach().numpy()
     
     red_colors = ['lightcoral', 'firebrick', 'darkred', 'red']
     blue_colors = ['lightblue', 'royalblue', 'darkblue', 'blue']
@@ -111,16 +129,18 @@ def plot_predictions(predictions, targets, test_losses, show=False):
     # TODO: Make sure this works... Might need to change the way errors are returned
     # Or the way they are plotted
     rmse, baseline_rmse, correlation = calculate_errors(predictions, targets)
+    correlations = correlation[1]
+    lags = correlation[0]
     
     # Plot each KPIs
     for i in range(len(axes)):
-        axes[i].title = axes[i].set_title(f'{KPI[i]}, RMSE: {rmse:.2f}, correlation: {correlation[1]:.2f}, baseline RMSE: {baseline_rmse:.2f}')
+        axes[i].title = axes[i].set_title(f'{KPI[i]}, RMSE: {rmse[i]:.2f}, correlation: {correlations[i]:.2f}, baseline RMSE: {baseline_rmse[i]:.2f}')
         axes[i].plot(predictions[:, i], label="predicted", color='indianred', alpha=.75)
         axes[i].plot(targets[:, i], label="target", color='royalblue', alpha=.75)
-        axes[i].plot(np.mean(targets, axis=1), label="mean target (baseline)", color='goldenrod', alpha=.75)
+        axes[i].plot(np.full(len(targets), np.mean(targets[:, i])), label="mean target (baseline)", linestyle='dotted', color='goldenrod', alpha=.75)
         axes[i].legend()
         
-    plt.suptitle(f'Predictions vs Targets, loss: {np.mean(test_losses):.2f}, RMSE: {rmse:.2f}, correlation: {np.mean(correlation[1]):.2f} baseline RMS: {np.mean(baseline_rmse):.2f}', fontsize=24)
+    plt.suptitle(f'Predictions vs Targets, loss: {np.mean(test_losses):.2f}, RMSE: {np.mean(rmse):.2f}, correlation: {np.mean(correlations):.2f} baseline RMSE: {np.mean(baseline_rmse):.2f}', fontsize=24)
     plt.tight_layout
     plt.savefig('reports/figures/model_results/predictions.pdf')
     if show:
@@ -133,34 +153,26 @@ def plot_predictions(predictions, targets, test_losses, show=False):
     start = 50
     end = 100
     
-    # TODO: Add this and make sure it works as above
-    return
     # Get zoomed in errors
     rmse, baseline_rmse, correlation = calculate_errors(predictions[start:end], targets[start:end])
+    correlations = correlation[1]
+    lags = correlation[0]
     
+    # Plot each KPIs
     for i in range(len(axes)):
-        # TODO: Correct correlation calculation
-        # calculate correlation between predictions and targets
-        correlation = np.corrcoef(targets[start:end, i], predictions[start:end, i])[0, 1]
-        
-        # calculate RMSE between predictions and targets
-        rmse= np.sqrt(np.mean(np.abs(targets[start:end, i] - predictions[start:end, i])**2))
-        
-        rmses.append(rmse)
-        correlations.append(correlation)
-        
-        axes[i].title = axes[i].set_title(f'{KPI[i]}, RMSE: {rmse:.2f}, correlation: {correlation:.2f}')
-        axes[i].plot(predictions[:, i], label="predicted", color='indianred', alpha=.75)
-        axes[i].plot(targets[:, i], label="target", color='royalblue', alpha=.75)
+        axes[i].title = axes[i].set_title(f'{KPI[i]}, RMSE: {rmse[i]:.2f}, correlation: {correlations[i]:.2f}, baseline RMSE: {baseline_rmse[i]:.2f}')
+        axes[i].plot(np.arange(start, end, step=1), predictions[:, i][start:end], label="predicted", color='indianred', alpha=.75)
+        axes[i].plot(np.arange(start, end, step=1), targets[:, i][start:end], label="target", color='royalblue', alpha=.75)
+        axes[i].plot(np.arange(start, end, step=1), np.full(len(targets[start:end]), np.mean(targets[:, i][start:end])), label="mean target (baseline)", linestyle='dotted', color='goldenrod', alpha=.75)
         axes[i].legend()
-        axes[i].set_xlim(start,end)
-    
-    plt.suptitle(f'Zoomed Predictions vs Targets, RMSE: {np.mean(rmses):.2f}, correlation: {np.mean(correlations):.2f}', fontsize=24)
+        
+    plt.suptitle(f'Zoomed Predictions vs Targets, RMSE: {np.mean(rmse):.2f}, correlation: {np.mean(correlations):.2f} baseline RMSE: {np.mean(baseline_rmse):.2f}', fontsize=24)
     plt.tight_layout
-    plt.savefig('reports/figures/model_results/predictions_zoomed.pdf')
+    plt.savefig(f'reports/figures/model_results/predictions_zoomed.pdf')
     if show:
         plt.show()
     plt.close()
+    
 
 def get_args():
     parser = ArgumentParser(description='Predict Model')
@@ -176,8 +188,11 @@ if __name__ == '__main__':
     model = HydraMRRegressor()
     model.load_state_dict(torch.load(args.model))
     
+    feature_extractors = ['MultiRocketMV_50000', 'HydraMV_8_64']
+    name_identifier = ''
+    
     # Load data
-    testset = Features(data_type='test')
+    testset = Features(data_type='test', feature_extractors=feature_extractors, name_identifier=name_identifier)
     test_loader = DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=0)
     
     predictions, targets, test_losses = predict(model, test_loader)
